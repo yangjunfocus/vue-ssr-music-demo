103453b25f2e179bdef7bbe2045e228c69a1c2dd
Zeroblank <mythyj61@163.com>
2018-09-11 15:07:17 +0800

    Update getters.js

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,8 @@ const getters = {
   getBookInfo2: state => state.bookInfo2,
   getRecom_Song_Menu: state => state.recom_song_menu,
   getNewest_Music: state => state.newest_music,
-  getHot_Music_List: state => state.hot_music_list
+  getHot_Music_List: state => state.hot_music_list,
+  getPlaylist_Detail: state => state.playlist_detail
 }
 
 
